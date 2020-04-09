83c259376eb164273afeba61d42402e608c84e6c
unlockedlabyoutube <57584655+unlockedlabyoutube@users.noreply.github.com>
2020-04-09 14:11:42 +0530

    Create sound.py

---
+++ sound.py
@@ -0,0 +1,43 @@
+from google.colab import output
+import cv2
+from darkflow.net.build import TFNet
+import numpy as np
+import time
+
+options = {
+    'model': 'cfg/yolov2-tiny.cfg',
+    'load': 'bin/yolov2-tiny_3000.weights',
+    'threshold': 0.1,
+}
+
+tfnet = TFNet(options)
+colors = [tuple(255 * np.random.rand(3)) for _ in range(10)]
+
+capture = cv2.VideoCapture("accident.mp4")    #change according to your video name
+capture.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
+capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
+
+while True:
+    stime = time.time()
+    ret, frame = capture.read()
+    if ret:
+        results = tfnet.return_predict(frame)
+        for color, result in zip(colors, results):
+            tl = (result['topleft']['x'], result['topleft']['y'])
+            br = (result['bottomright']['x'], result['bottomright']['y'])
+            label = result['label']
+            confidence = result['confidence']
+            text = '{}: {:.0f}%'.format(label, confidence * 100)
+            frame = cv2.rectangle(frame, tl, br, color, 5)
+            frame = cv2.putText(
+                frame, text, tl, cv2.FONT_HERSHEY_COMPLEX, 1, (0, 0, 0), 2)
+        cv2.imshow('frame', frame)
+        if label=='accident':                       #change according to your lable name
+          print("accident detected")
+          output.eval_js('new Audio("https://upload.wikimedia.org/wikipedia/commons/0/05/Beep-09.ogg").play()')
+          break
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
